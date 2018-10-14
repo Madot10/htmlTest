f402d399405083159a9ab8c4bf8a2bdc57f68757
Madot <migueldeolim1@gmail.com>
2018-10-14 15:32:07 -0400

    News events

--- js/fun.js
+++ js/fun.js
@@ -86,30 +86,6 @@ window.onload = function() {
 
 }
 
-//This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
-
-//Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
-if (navigator.serviceWorker.controller) {
-    console.log('[PWA Builder] active service worker found, no need to register')
-  } else {
-  
-  //Register the ServiceWorker
-    navigator.serviceWorker.register('uc-sw.js', {
-      scope: '/htmlTest/'
-    }).then(function(reg) {
-      console.log('Service worker has been registered for scope:'+ reg.scope);
-    });
-  }
-  
-  // reemplazar 'standalone' por 'fullscreen' o 'minimal-ui' según el manifest de la app
-if (matchMedia('(display-mode: standalone)').matches) {
-    // iOS 11.3+ y Android
-    isInBrowser = "Pwa";
-  } else if ('standalone' in navigator) {
-    // para iOS &lt; 11.3
-    isInBrowser = "Pwa";
-  }
-  
 
 function DiasPasados() {
     f1 = '28/02/2018';
